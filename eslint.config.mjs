44938013a6735c9a53141354ffde7ef69712b16a
git-leo-here <ankit2003.ar15@gmail.com>
2025-07-20 22:08:30 +0530

    ESLint config v3.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,17 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-  rules: {
-        "@typescript-eslint/no-unused-vars": 1,
-        "@typescript-eslint/no-explicit-any": 1,
-        "react/no-unescaped-entities" : 1,
-        "@typescript-eslint/no-empty-object-type": 1,
-      },
-  }
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//   rules: {
+//         "@typescript-eslint/no-unused-vars": 1,
+//         "@typescript-eslint/no-explicit-any": 1,
+//         "react/no-unescaped-entities" : 1,
+//         "@typescript-eslint/no-empty-object-type": 1,
+//         "@typescript-eslint/ban-types": 0,
+//         "@typescript-eslint/explicit-module-boundary-types": 0
+//       },
+//   }
   
-];
+// ];
+
+const eslintConfig = []
 
 export default eslintConfig;
